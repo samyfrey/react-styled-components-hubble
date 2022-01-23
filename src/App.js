bb7fc64fb8375ebca035d9336b8ded636935c3be
samyfrey <samyfrey@yahoo.com>
2022-01-23 17:11:10 -0500

    theme and global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { ThemeProvider } from 'styled-components'
 import { Container } from './components/styles/Container.styled'
+import Header from './components/Header'
+import GlobalStyles from './components/styles/Global'
+
+// to create our personal theme 
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333'
+  }
+}
 
 function App() {
   return (
+    <ThemeProvider theme = {theme}>
+    <>
+    <GlobalStyles />
+    <Header />
     <Container>
       <h1>Hello World</h1>
     </Container>
+    </>
+    </ThemeProvider>
   );
 }
 
